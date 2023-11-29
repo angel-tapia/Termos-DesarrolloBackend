cc7fe8eddd3a6133ce878a3dcde62c62a0c1d356
Angel Manuel Tapia Avitia <angel@Angels-MacBook.local>
2023-11-29 17:31:23 -0600

    Probado CRUD Usuario

--- springboot-service-usuarios/src/main/resources/import.sql
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -1,2 +1,9 @@
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', 'abcde', true, 'Admin', 'Admin', 'admin@localhost');
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('angel', 'abcde', true, 'Angel', 'Tapia', 'angel@tapia.com');
+
+INSERT INTO roles (nombre) VALUES('ROLE_USER')
+INSERT INTO roles (nombre) VALUES('ROLE_ADMIN')
+
+INSERT INTO usuarios_to_roles (user_id, rooles_id) VALUES(1, 1)
+INSERT INTO usuarios_to_roles (user_id, rooles_id) VALUES(2, 2)
+INSERT INTO usuarios_to_roles (user_id, rooles_id) VALUES(2, 1)
