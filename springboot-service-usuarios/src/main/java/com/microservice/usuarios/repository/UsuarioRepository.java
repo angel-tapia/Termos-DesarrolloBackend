691d4c2a3dd254fa664cb4d094be1f9bb4c8561b
Angel Manuel Tapia Avitia <angel@Angels-MacBook.local>
2023-11-28 23:46:26 -0600

    Creando repository usuarios

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
@@ -1,5 +1,23 @@
 package com.microservice.usuarios.repository;
 
-public class UsuarioRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.*;
 
+import com.microservice.usuarios.entity.*;
+
+public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long>{
+	
+	
+	// SELECT u from Usuario  u where u.username = ?1
+	public Usuario findByUsername(String username); 
+	
+	// SELECT u from Usuario  u where u.username = ?1 and u.email = ?2
+	public Usuario findByUsernameAndEmail(String username, String email);
+	
+	@Query(value = "SELECT u from Usuario  u where u.username = ?1")
+	public Usuario obtenerPorUsername(String username);
+	
+	@Query(value = "SELECT u from Usuario  u where u.username = ?1 and u.email = ?2")
+	public Usuario obtenerPorUsernameYEmail(String username, String email);
+	
 }
