1fdb31ba5cfbb0829cc76a56bd887d39cad75dfb
Angel Manuel Tapia Avitia <angel@Angels-MacBook.local>
2023-11-28 19:54:51 -0600

    Generando entidades usuario y rol

---
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/entity/Rol.java
@@ -0,0 +1,40 @@
+package com.microservice.usuarios.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="roles")
+public class Rol {
+
+	private static final long serialVersionUID = 8418249012312L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true, length = 100)
+	private String nombre;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
