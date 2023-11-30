aa14557f03d17d416fbae9a05206428d46f7c605
Angel Manuel Tapia Avitia <angel@Angels-MacBook.local>
2023-11-30 16:40:52 -0600

    Configurando clientes

--- springboot-service-oauth/src/main/java/com/microservice/oauth/security/AuthorizationServerConfig.java
+++ springboot-service-oauth/src/main/java/com/microservice/oauth/security/AuthorizationServerConfig.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -17,13 +20,40 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
-	@Override
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Override //endpoint /oauth/token POST username, password, client id, claims
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authenticationManager)
 		.tokenStore(jwtTokenStore())
 		.accessTokenConverter(jwtAccessTokenConverter());
 	}
 	
+	
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()") // /oauth/token
+		.checkTokenAccess("isAuthenticated()");
+	}
+
+
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("backEndApp")
+		.secret(passwordEncoder.encode("contra123"))
+		.scopes("read", "write")
+		.authorizedGrantTypes("password", "refresh_token")
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
+		
+		//Aqui se pueden agregar mas cleintes
+	}
+
+
+
 	@Bean
 	public JwtTokenStore jwtTokenStore() {
 		return new JwtTokenStore(jwtAccessTokenConverter());
