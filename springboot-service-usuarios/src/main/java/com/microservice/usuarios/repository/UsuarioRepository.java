8168a5e14efe8b787319d26774a9d7a88aba053c
Angel Manuel Tapia Avitia <angel@Angels-MacBook.local>
2023-11-29 19:49:32 -0600

    RestResource Personalizado

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
@@ -2,7 +2,9 @@ package com.microservice.usuarios.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.microservice.usuarios.entity.Usuario;
 
@@ -12,7 +14,8 @@ public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, L
 	
 	
 	// SELECT u from Usuario  u where u.username = ?1
-	public Usuario findByUsername(String username); 
+	@RestResource(path = "buscar-username")
+	public Usuario findByUsername(@Param("nombre") String username); 
 	
 	// SELECT u from Usuario  u where u.username = ?1 and u.email = ?2
 	public Usuario findByUsernameAndEmail(String username, String email);
